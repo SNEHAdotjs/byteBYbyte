80054972e7aa63427edd19b93f2fbd5ce5084a99
SNEHAdotjs <snehaaa7002@gmail.com>
2025-12-18 11:19:50 +0530

    Day 5 - Arrays practice

---
+++ projects/day5-arrays.js
@@ -0,0 +1,7 @@
+let skills = ["HTML", "CSS", "JavaScript", "React"];
+
+console.log("My skills:");
+console.log(skills);
+
+console.log("First skill:", skills[0]);
+console.log("Total skills:", skills.length);
