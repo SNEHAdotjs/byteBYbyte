98bb4151668d90b451ba7ac595a21783527f56eb
SNEHAdotjs <snehaaa7002@gmail.com>
2025-12-18 11:22:28 +0530

    Day 6 - Objects practice

---
+++ projects/day6-objects.js
@@ -0,0 +1,10 @@
+let student = {
+  name: "Sneha",
+  role: "Aspiring Full Stack Developer",
+  skills: ["HTML", "CSS", "JavaScript"]
+};
+
+console.log(student);
+console.log("Name:", student.name);
+console.log("Role:", student.role);
+console.log("First skill:", student.skills[0]);
